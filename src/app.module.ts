62a9d938ffa9238a6bca09a8a2a04598281a4163
NongEs <oootengi@gmail.com>
2025-07-24 05:38:56 +0700

    -update users resources
    -update users resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'data.db',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User])
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
